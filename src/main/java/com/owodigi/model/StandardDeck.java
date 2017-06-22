a4853b87b43551bd6215610658dd21c1fe71f9e4
Bobby Owolabi <bobbyowolabi@gmail.com>
2017-06-21 21:42:02 -0400

    passed 53rd card condition

--- src/main/java/com/owodigi/model/StandardDeck.java
+++ src/main/java/com/owodigi/model/StandardDeck.java
@@ -1,11 +1,19 @@
 package com.owodigi.model;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  *
  */
 class StandardDeck implements Deck {
+    private List<Card> cards = new ArrayList<>();
 
     public StandardDeck() {
+        for (int i = 0; i < 52; ++i) {
+            cards.add(i, new Card());
+        }
     }
 
     @Override
@@ -15,6 +23,12 @@ class StandardDeck implements Deck {
 
     @Override
     public Card dealOneCard() {
-        return new Card();
+        final Iterator<Card> iterator = cards.iterator();
+        if (iterator.hasNext()) {
+            final Card card = iterator.next();
+            iterator.remove();
+            return card;
+        }
+        return null;        
     }
 }
