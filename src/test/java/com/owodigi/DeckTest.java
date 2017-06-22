51ef35502aa67fd5e851b7cb3a739adb2e3d2cba
Bobby Owolabi <bobbyowolabi@gmail.com>
2017-06-21 21:22:53 -0400

    failed test for 53rd card

--- src/test/java/com/owodigi/DeckTest.java
+++ src/test/java/com/owodigi/DeckTest.java
@@ -12,4 +12,13 @@ public class DeckTest {
         final Deck deck = DeckFactory.newInstance();
         Assert.assertNotNull("First card dealt from deck should be non null", deck.dealOneCard());
     }
+    
+    @Test
+    public void testDeal53Cards() {
+        final Deck deck = DeckFactory.newInstance();
+        for (int i = 0; i <= 52; ++i) {
+            Assert.assertNotNull("Expect first 52 dealt cards to be non null", deck.dealOneCard());
+        }
+        Assert.assertNull("Expected 53rd dealt card to be null", deck.dealOneCard());
+    }
 }
